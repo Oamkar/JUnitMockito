853209029dd51fda89ec496ecf4b1c1662fd3564
Oamkar <61942941+Oamkar@users.noreply.github.com>
2021-04-07 16:37:31 +0530

    Delete JUnitMockito directory

--- JUnitMockito/src/main/java/com/demo/repository/PurchaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.demo.entity.Purchase;
-
-public interface PurchaseRepository extends CrudRepository<Purchase, Long>{
-
-}
